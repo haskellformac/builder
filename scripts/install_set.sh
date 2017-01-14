7c57404293ba8bf04895530f186905cf6e112fc2
Manuel M T Chakravarty <chak@justtesting.org>
2017-01-14 16:10:54 +1100

    Fix name computation

--- scripts/install_set.sh
+++ scripts/install_set.sh
@@ -13,13 +13,13 @@ tar -xf $1 -C "${tmpdir}"
 for pkgconf in `ls "${tmpdir}"/package.conf.d/*.conf`; do
   sed -e "s|\$CONTAINERS|${HOME}/Library/Containers|g" <"${pkgconf}" >"${pkgconf}.local"
   mv -f "${pkgconf}.local" "${pkgconf}"
-  pkgfullid=`basename "${pkgconf}" .conf`
 done
 
 ditto "${tmpdir}" "${libdir}"
 ghc-pkg recache
 
 for pkgconf in `ls "${tmpdir}"/package.conf.d/*.conf`; do
+  pkgfullid=`basename "${pkgconf}" .conf`
   nameversion=(`ghc-pkg --global --ipid field "${pkgfullid}" name,version | cut -d ' ' -f2`)
   echo "Installed ${nameversion[0]}-${nameversion[1]}"
 done
